6e8e38f90401bc81a64d8c380006659d80524332
Slye <=>
2021-11-08 13:00:28 +0000

    hot fixes: fix compile warnings

--- dev.mycontributionsapp.com/src/pages/login.js
+++ dev.mycontributionsapp.com/src/pages/login.js
@@ -5,53 +5,53 @@ class Login extends React.Component {
     return (
       <div>
         <div id="app">
-          <div id="containerbar" class="containerbar authenticate-bg">
-            <div class="container">
-              <div class="auth-box login-box center">
-                <div class="row no-gutters align-items-center justify-content-center">
-                  <div class="auth-box-right">
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="form-head">
-                          <a href="#" class="logo">
+          <div id="containerbar" className="containerbar authenticate-bg">
+            <div className="container">
+              <div className="auth-box login-box center">
+                <div className="row no-gutters align-items-center justify-content-center">
+                  <div className="auth-box-right">
+                    <div className="card">
+                      <div className="card-body">
+                        <div className="form-head">
+                          <a href="/" className="logo">
                             <img
                               src="assets/img/logo177.png"
                               alt="logo"
-                              class="img-fluid"
+                              className="img-fluid"
                             />
                           </a>
                         </div>
 
                         <form>
-                          <h4 class="form-title">Log in !</h4>
-                          <div class="form-group">
+                          <h4 className="form-title">Log in !</h4>
+                          <div className="form-group">
                             <input
                               type="text"
                               placeholder="phone"
                               required
-                              class="form-control"
+                              className="form-control"
                             />
                           </div>
 
-                          <div class="form-group">
+                          <div className="form-group">
                             <input
                               type="password"
                               placeholder="password"
-                              minlength="8"
+                              minLength="8"
                               required
-                              class="form-control"
+                              className="form-control"
                             />
                           </div>
-                          <button class="btn">log in</button>
+                          <button className="btn">log in</button>
 
-                          <div class="form-row">
-                            <div class="forgot-psw">
-                              <a href="#">Forgot Password?</a>
+                          <div className="form-row">
+                            <div className="forgot-psw">
+                              <a href="#forgot-password">Forgot Password?</a>
                             </div>
                           </div>
-                          <p class="sign-up">
+                          <p className="sign-up">
                             Don't have an account?
-                            <a href="#"> Sign Up</a>
+                            <a href="#sign-up"> Sign Up</a>
                           </p>
                         </form>
                       </div>
